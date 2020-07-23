b1bdaab7738206e1e02a5219cbbe9bee9c1bc06d
Illia Hreben <muomieg@gmail.com>
2020-07-23 21:33:41 +0300

    added history of weather search by city

--- client/public/app.js
+++ client/public/app.js
@@ -2,20 +2,39 @@ document.getElementById('search').onclick = () => {
 
 const cityName = document.getElementById('cityName').value
 
+  window.fetch(`/api/showhistory?cityName=${cityName}`)
+    .then(function (res) {
+      return res.text()
+        .then(JSON.parse)
+        .then(body => {
+          if (body.ok) {
+            return body.data
+          }
+          throw body.error
+        })
+    })
+    .then(data => {
+      data[0].forEach((row) => {
+        const textDate = `${row.temp}\u2103 (${formatDate(row.datetime)})`
+        displayTempToUser(textDate, 'weatherHistory')
+        console.log(typeof row.datetime)
+      })
+    })
+
   window.fetch(`/api/accu?cityName=${cityName}`)
-      .then(function (res) {
-        return res.text()
-          .then(JSON.parse)
-          .then(body => {
-            if (body.ok) {
-              return body.data
-            }
-            throw body.error
-          })
-      })
-      .then(function (data) {
-        displayTempToUser(data[0].Temperature.Metric.Value, 'accuweather')
-      })
+    .then(function (res) {
+      return res.text()
+        .then(JSON.parse)
+        .then(body => {
+          if (body.ok) {
+            return body.data
+          }
+          throw body.error
+        })
+    })
+    .then(function (data) {
+      displayTempToUser(data[0].Temperature.Metric.Value + '\u2103', 'accuweather')
+    })
 
   window.fetch(`/api/open?cityName=${cityName}`)
     .then(function (res) {
@@ -29,33 +48,48 @@ const cityName = document.getElementById('cityName').value
         })
     })
     .then(function (data) {
-      const tempInCelsius = data.list[0].main.temp - 273.15
-      displayTempToUser(+tempInCelsius.toFixed(1), 'openweather')
+      displayTempToUser(data.list[0].main.temp + '\u2103', 'openweather')
     })
 
     window.fetch(`/api/yahoo?cityName=${cityName}`)
-      .then(function (res) {
-        return res.text()
-          .then(JSON.parse)
-          .then(body => {
-            if (body.ok) {
-              return body.data
-            }
-            throw body.error
-          })
+      .then(res => res.text())
+      .then(JSON.parse)
+      .then(body => {
+        if (body.ok) {
+          return body.data
+        }
+        throw body.error
       })
       .then(data => {
-        displayTempToUser(data.current_observation.condition.temperature, 'yahooweather')
+        displayTempToUser(data.current_observation.condition.temperature + '\u2103', 'yahooweather')
       })
 }
 
-  function displayTempToUser (value, divTempId) {
-    const newDiv = document.createElement('input')
-    newDiv.type = 'text'
-    newDiv.value = value
-    const divDisplayWeather = document.getElementById(divTempId)
-    divDisplayWeather.appendChild(newDiv)
-  }
+function displayTempToUser (value, divTempId) {
+  const newDiv = document.createElement('input')
+  newDiv.type = 'text'
+  newDiv.value = value
+  const divDisplayWeather = document.getElementById(divTempId)
+  divDisplayWeather.appendChild(newDiv)
+}
+
+function formatDate (dateStr) {
+  const dateObj = new Date(dateStr)
+
+  const formatedTime = [
+    dateObj.getHours(),
+    dateObj.getMinutes(),
+    dateObj.getSeconds()
+  ].join(':')
+
+  const formatedDate = [
+    dateObj.getDate(),
+    dateObj.getMonth() + 1,
+    dateObj.getFullYear()
+  ].join('.')
+
+  return `${formatedTime}, ${formatedDate}`
+}
 
 // [
 //   {
